498417ac5f118322cdf6c81be576fdca32adc8ea
k_o_ <k_o_@f4f2cb91-632f-4d2c-bd03-4b3085b5da43>
2009-11-02 02:27:48 +0000

    Fork mode in test reenabled
    git-svn-id: http://svn.code.sf.net/p/globalplatform/code@207 f4f2cb91-632f-4d2c-bd03-4b3085b5da43

--- branches/GlobalPlatform-6.0.0/main/GlobalPlatformTest.c
+++ branches/GlobalPlatform-6.0.0/main/GlobalPlatformTest.c
@@ -216,7 +216,6 @@ int main (void)
 	int number_failed;
 	Suite *s = GlobalPlatform_suite ();
 	SRunner *sr = srunner_create (s);
-	srunner_set_fork_status (sr, CK_NOFORK);
 	srunner_run_all (sr, CK_NORMAL);
 	number_failed = srunner_ntests_failed (sr);
 	srunner_free (sr);
